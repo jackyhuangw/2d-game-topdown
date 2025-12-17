db82fdbb931b83f20de68498b29fb7a635297202
jackyhuangw <134223686+jackyhuangw@users.noreply.github.com>
2025-12-17 23:55:51 +0700

    added enemy prefabs, enemy destroy mechanic and death effect on enemy

---
+++ Assets/Scripts/Utills/AnimatedObjectDestroy.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class AnimatedObjectDestroy : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    void Start()
+    {
+        Destroy(gameObject, animator.GetCurrentAnimatorStateInfo(0).length);
+    }
+}
