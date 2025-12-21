64005017ebbee89993d68e083b58edcf80d12f47
jackyhuangw <134223686+jackyhuangw@users.noreply.github.com>
2025-12-21 20:12:02 +0700

    add song to the game and menu

---
+++ Assets/Scripts/Utills/MenuManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void NewGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
